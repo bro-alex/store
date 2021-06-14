ec6a79aef4ebe6c2fd111778b98e03d2b6c446e2
bro-alex <br.terehov@gmail.com>
2021-06-14 22:35:01 +0200

    step 1.4 finished

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \store\App();
 
-throw new Exception('Страница не найдена', '404');

59f08415abda279ecb93de71a1be643b7fea6ac6
bro-alex <br.terehov@gmail.com>
2021-06-25 18:25:41 +0200

    step 1.10 finish

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,6 +6,10 @@ class MainController extends AppController{
 
     public function indexAction(){
 
-        echo __METHOD__;
+        //echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $name = 'John';
+        $age = 30;
+        $this->set(compact('name', 'age' , 'names'));
     }
 }
--- vendor/store/core/base/Controller.php
+++ vendor/store/core/base/Controller.php
@@ -9,8 +9,9 @@ abstract class Controller{
     public $model;
     public $view;
     public $prefix;
+    public $layout;
     public $data = [];
-    public $meta = [];
+    public $meta = ['title' => '', 'desc' => '', 'keywords' => ''];
 
     public function __construct($route){
         $this->route = $route;
@@ -20,6 +21,12 @@ abstract class Controller{
         $this->prefix = $route['prefix'];
 
     }
+
+public function getView(){
+    $viewObject = new View($this->route, $this->layout, $this->view, $this->meta);
+    $viewObject->render($this->data);
+}
+
     public function set($data){
         $this->data = $data;
     }

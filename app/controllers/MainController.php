335d599102cad9525b01e379b1de0c76d9580615
bro-alex <br.terehov@gmail.com>
2021-06-29 18:18:03 +0200

    1 step all finish

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use store\Cache;
+
 class MainController extends AppController{
 
     public function indexAction(){
@@ -12,6 +14,11 @@ class MainController extends AppController{
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
         $name = 'John';
         $age = 30;
+        $names = ['Andrey', 'Jane', 'Mike'];
+        $cache = Cache::instance();
+        //$cache->set('test', $names);
+        $data = $cache->get('test');
+        debug($data);
         $this->set(compact('name', 'age' , 'names', 'posts'));
     }
 }

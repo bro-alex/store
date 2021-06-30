1d64fdda97794375b669a58081c16060bb01f796
bro-alex <br.terehov@gmail.com>
2021-06-30 09:48:48 +0200

    finish 1 block

--- config/config_db.php
+++ config/config_db.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'dsn' => 'mysql:host=db4free.net;dbname=f0554939_store;charset=utf8',
-    'user' => 'f0554939_store',
-    'pass' => 'Urban2013+',
+    'dsn' => 'mysql:host=mydbtest.ml;dbname=f0554939_storetest;charset=utf8',
+    'user' => 'f0554939_storetest',
+    'pass' => 'cSmMNmml',
 ];
